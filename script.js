cb2f1b3ce3432e3724644a8c71c7930e6fc76da2
spdc-elm <58126622+spdc-elm@users.noreply.github.com>
2023-04-22 21:52:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const switchButton = document.querySelector('#switch');
+const new_old_button = document.querySelector('#new_old');
 let jump = false;
-
+let old = false;
 switchButton.addEventListener('click', () => {
   if (jump) {
     switchButton.innerText = 'Off';
@@ -10,40 +11,45 @@ switchButton.addEventListener('click', () => {
     jump = true;
   }
 })
+new_old_button.addEventListener('click', () => {
+  if (old) {
+    switchButton.innerText = 'new';
+    old = false;
+  } else {
+    switchButton.innerText = 'old'; 
+    old = true;
+  }
+})
 
-function run(){
-	var names = document.querySelector('textarea').value;
+function get_url(names){
 	var base64 = window.btoa(unescape(encodeURIComponent(names))).replace(/\+/g,'-').replace(/\//g,'_');
-	url='https://deepmess.com/namerena/#n=' + base64;
+	var prefix = old ? 'https://xyf0076.gitee.io/namerena.github.io/#n=' : 'https://deepmess.com/namerena/#n='
+	url= prefix + base64;
+	return url;
+}
+function battle(names){
+	url = get_url(names);
 	if (jump){
 		location.href=url;
 	} else {
 		document.querySelector('iframe').src = url;
 	}
+}
+function run(){
+	var names = document.querySelector('textarea').value;
+	battle(names);
 	
 }
 	
 function test(){
 	var names = "!test!\n!\n\n" + document.querySelector('textarea').value;
-	var base64 = window.btoa(unescape(encodeURIComponent(names))).replace(/\+/g,'-').replace(/\//g,'_');
-	url='https://deepmess.com/namerena/#n=' + base64;
-	if (jump){
-		location.href=url;
-	} else {
-		document.querySelector('iframe').src = url;
-	}
+	battle(names);
 }
 	
 function seed(){
 	let timestamp = new Date().toISOString();
 	var names = document.querySelector('textarea').value+"\n\nseed:"+timestamp+"@!";
-	var base64 = window.btoa(unescape(encodeURIComponent(names))).replace(/\+/g,'-').replace(/\//g,'_');
-	url='https://deepmess.com/namerena/#n=' + base64;
-	if (jump){
-		location.href=url;
-	} else {
-		document.querySelector('iframe').src = url;
-	}
+	battle(names);
 }
 function random_enemy(){
 	let selector = document.querySelector('#enemy_type');
@@ -61,18 +67,30 @@ function random_enemy(){
 		console.log(random_name)
 		let timestamp = new Date().toISOString();
 		var names = document.querySelector('textarea').value+"\n\n"+random_name+"\n\nseed:"+timestamp+"@!";
-		var base64 = window.btoa(unescape(encodeURIComponent(names))).replace(/\+/g,'-').replace(/\//g,'_');
-		url='https://deepmess.com/namerena/#n=' + base64;
-		if (jump){
-			location.href=url;
-		} else {
-			document.querySelector('iframe').src = url;
-		}
+		battle(names);
 	  }
 	};
 	xhr.send();
 	
 }
+function multirounds(rounds){
+	/*生成多轮对战的url链接文本，输出*/
+	var nametext = document.querySelector('textarea').value;
+	var urls = [];
+	for (i=0; i<rounds; i++){
+		let timestamp = new Date().toISOString();
+		var names = nametext + "\n\nseed:"+timestamp+"@!";
+		urls.push(get_url(names));
+	}
+	
+	var str=rounds + '轮对战的链接如下：\n';
+	for (i=0; i<rounds; i++){
+		str += '第${i+1}轮: ${urls[i]}\n';
+	}
+	
+	document.querySelector('textarea[readonly]').value = str;
+}
+
 function onMessage(e) {
 	console.log(JSON.stringify(e.data, null, ' '));
 	document.querySelector('textarea[readonly]').value = e.data.winners.join('\n');
